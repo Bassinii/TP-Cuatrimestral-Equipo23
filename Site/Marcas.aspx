14987419f374742edd75970c0046f85601a472cf
DanielaBentancor <167239344+DanielaBentancor@users.noreply.github.com>
2024-06-08 12:04:34 -0300

    Ventana Gestion de Marcas
    -Ver Marcas
    -Agregar , Modificar
    -Sin funcionalidad

---
+++ Site/Marcas.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Marcas.aspx.cs" Inherits="Site.Marcas" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Gestión de Marcas</title>
+</head>
+<body>
+    <h1>Gestión de Marcas</h1>
+    <form id="form1" runat="server">
+         <!-- ver marcas -->
+        <div>
+            <h2>Marcas:</h2>
+            <p>se muestra una grilla con las marcas existentes</p>
+        </div>
+        <!-- agregar y modificar marcas -->
+         <div>
+            <h2><asp:Label ID="LblTituloFormulario" runat="server" Text="Agregar Nueva Marca"></asp:Label></h2>
+            <asp:HiddenField ID="HiddenFieldIDMarca" runat="server" />
+            <label>Nombre</label>
+            <asp:TextBox ID="TextBoxNombre" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="BtnGuardar" runat="server" Text="Guardar" />
+            <asp:Button ID="BtnCancelar" runat="server" Text="Cancelar" Visible="true" />
+        </div>
+    </form>
+    <asp:HyperLink ID="LinkVolver" runat="server" NavigateUrl="default.aspx">Volver a Inicio</asp:HyperLink>
+</body>
+</html>
---
+++ Site/Marcas.aspx.designer.cs
@@ -0,0 +1,80 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Site
+{
+
+
+    public partial class Marcas
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control LblTituloFormulario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblTituloFormulario;
+
+        /// <summary>
+        /// Control HiddenFieldIDMarca.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField HiddenFieldIDMarca;
+
+        /// <summary>
+        /// Control TextBoxNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxNombre;
+
+        /// <summary>
+        /// Control BtnGuardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnGuardar;
+
+        /// <summary>
+        /// Control BtnCancelar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnCancelar;
+
+        /// <summary>
+        /// Control LinkVolver.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink LinkVolver;
+    }
+}
