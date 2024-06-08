c6afd2996443eb832380ae5a45e138e7b75b367c
DanielaBentancor <167239344+DanielaBentancor@users.noreply.github.com>
2024-06-08 12:43:46 -0300

    Ventana Ventas
    -Generar Venta 
    -Sin funcionalidad

---
+++ Site/Ventas.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Ventas.aspx.cs" Inherits="Site.Ventas" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Gestión de Ventas</title>
+</head>
+<body>
+    <h1>Gestión de Ventas</h1>
+    <form id="form1" runat="server">
+        <!-- nueva venta -->
+    <div>
+    <h2>Nueva Venta</h2>
+            <label>Empleado</label>
+            <p>se muestra DropDownListEmpleado existentes</p>
+            <br />
+            <label>Cliente</label>
+            <p>se muestra DropDownListClientes existentes</p>
+             <br />
+            <p>se autocompletan automaticamente cuando se agregue la funcionalidad</p>
+            <label>Nro de Factura: </label>
+            <asp:TextBox ID="TextBoxFactura" runat="server" ReadOnly="true"></asp:TextBox>
+            <br />
+            <label>Fecha</label>
+            <asp:TextBox ID="TextBoxFecha" runat="server" ReadOnly="true"></asp:TextBox>
+            <br />
+            <label>Hora</label>
+            <asp:TextBox ID="TextBoxHora" runat="server" ReadOnly="true"></asp:TextBox>
+            <br />
+            <h3>Detalles de la Venta</h3>
+            <p>Cargamos datos de la venta: articulo, cantidad, precio.</p>
+            <asp:Button ID="BtnAgregarDetalle" runat="server" Text="Agregar Detalle" Visible="false" />
+            <br /><br />
+            <label>Subtotal</label>
+            <asp:TextBox ID="TextBoxSubtotal" runat="server"></asp:TextBox>
+            <br />
+            <label>Total</label>
+            <asp:TextBox ID="TextBoxTotal" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="BtnGuardarVenta" runat="server" Text="Generar Venta" />
+            <asp:Button ID="BtnCancelar" runat="server" Text="Cancelar" Visible="true" />
+        </div>
+    </form>
+    <asp:HyperLink ID="LinkVolver" runat="server" NavigateUrl="default.aspx">Volver a Inicio</asp:HyperLink>
+</body>
+</html>
---
+++ Site/Ventas.aspx.designer.cs
@@ -0,0 +1,107 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Site
+{
+
+
+    public partial class Ventas
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control TextBoxFactura.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxFactura;
+
+        /// <summary>
+        /// Control TextBoxFecha.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxFecha;
+
+        /// <summary>
+        /// Control TextBoxHora.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxHora;
+
+        /// <summary>
+        /// Control BtnAgregarDetalle.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnAgregarDetalle;
+
+        /// <summary>
+        /// Control TextBoxSubtotal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxSubtotal;
+
+        /// <summary>
+        /// Control TextBoxTotal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxTotal;
+
+        /// <summary>
+        /// Control BtnGuardarVenta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnGuardarVenta;
+
+        /// <summary>
+        /// Control BtnCancelar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnCancelar;
+
+        /// <summary>
+        /// Control LinkVolver.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink LinkVolver;
+    }
+}
