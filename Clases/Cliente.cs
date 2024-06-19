6d1fd028a6dd69b571b47bd93d92fd4a63f86750
DanielaBentancor <167239344+DanielaBentancor@users.noreply.github.com>
2024-06-19 00:53:22 -0300

    ABM Clientes

--- Clases/Cliente.cs
+++ Clases/Cliente.cs
@@ -6,9 +6,13 @@ using System.Threading.Tasks;
 
 namespace Clases
 {
-    public class Categoria
+    public class Cliente
     {
         public int id { get; set; }
         public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string direccion { get; set; }
+        public string mail { get; set; }
+        public string telefono { get; set; }
     }
 }
