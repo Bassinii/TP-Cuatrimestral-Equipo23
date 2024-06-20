9414998ef694f3ee14543a30eab046333d59ae9c
DanielaBentancor <167239344+DanielaBentancor@users.noreply.github.com>
2024-06-20 16:49:00 -0300

    Gestión Clientes Validaciones

---
+++ Negocio/Validacion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Negocio
+{
+    public static class Validacion
+    {
+        public static bool validaTextoVacio(object control)
+        {
+           
+            if(control is TextBox textBox)
+            {
+                //TextBox textBox = (TextBox)control;
+                return !string.IsNullOrEmpty(textBox.Text);
+            }
+            return false;
+        }
+    }
+}
