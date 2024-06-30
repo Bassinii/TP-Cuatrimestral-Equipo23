7385ed65c221aea96941e70c945c453bf68e8c31
DanielaBentancor <167239344+DanielaBentancor@users.noreply.github.com>
2024-06-29 23:49:30 -0300

    NegocioUsuario

---
+++ Negocio/NegocioUsuario.cs
@@ -0,0 +1,42 @@
+﻿using Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class NegocioUsuario
+    {
+        public bool Loguear(Usuario usuario)
+        {
+            AccesoDatos datos= new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("select ID_Usuario,TipoUsuario from Usuarios where Usuario= @usuario and Contrasenia = @contrasenia");
+                datos.setearParametro ("@usuario", usuario.UsuarioNombre);
+                datos.setearParametro("@contrasenia", usuario.Contrasenia);
+
+                datos.ejecutarLectura();
+                while(datos.Lector.Read())
+                {
+                    usuario.ID_Usuario = (int)datos.Lector["ID_Usuario"];
+                    usuario.TipoUsuario = (int)(datos.Lector["TipoUsuario"]) == 1 ? TipoUsuario.VENTAS : TipoUsuario.ADMIN;
+                    return true;
+                }
+                return false;
+            }
+
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+        }
+    }
+}
